bba9ebc150772ce348879bd12e8901afbf9dd2c9
Oscar Gichana <ossygicchy@gmail.com>
2021-04-11 09:25:43 +0300

    adding registration and login templates

--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tracker.apps.TrackerConfig',
     'bootstrap3',
+    'django_registration',
 ]
 
 MIDDLEWARE = [
--- project/urls.py
+++ project/urls.py
@@ -19,5 +19,8 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('tracker.urls')),
+    path('^accounts/', include('registration.backends.simple.urls')),
+    path('^accounts/', include('django.contrib.auth.urls')),
+    path('^logout/$', views.LogoutView.as_view()),
 
 ]
