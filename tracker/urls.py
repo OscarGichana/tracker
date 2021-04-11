04f45afa94b7734c8f8cf6f507aee98996ff61cf
Oscar Gichana <ossygicchy@gmail.com>
2021-04-11 08:53:23 +0300

    adding url to index page

---
+++ tracker/urls.py
@@ -0,0 +1,28 @@
+from django.conf.urls import url,include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
+urlpatterns=[
+    url(r'^$',views.index,name='index'),
+    # url(r'^profile',views.profile,name = 'profile'),
+    # url(r'^project/(\d+)', views.project, name='project'),
+    # url(r'^$', views.review_list, name='review_list'),
+    # url(r'^review/(?P<review_id>[0-9]+)/$',
+    #     views.review_detail, name='review_detail'),
+    # url(r'^new/project$', views.new_project, name='new-project'),
+    # url(r'^search/', views.search_projects, name='search_projects'),
+    # url(r'^edit_profile',views.edit_profile,name = 'edit-profile'),
+    # url(r'^new_profile/$',views.new_profile,name = 'new_profile'),
+    # url(r'^api/awmerch/$', views.MerchList.as_view()),
+    # url(r'^api/awproject/$', views.ProjectList.as_view()),
+    # url(r'api/merch/merch-id/(?P<pk>[0-9]+)/$',views.MerchDescription.as_view()),
+    # url(r'api/prod/prod-id/(?P<pk>[0-9]+)/$',views.ProductDescription.as_view()),
+
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    # projects = Project.get_all_projects()
+    # locations = Location.objects.all()
+    title = 'O_world'
+    return render(request, 'index.html', )
