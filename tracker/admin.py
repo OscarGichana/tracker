e9a447fa6f83346c9a51cf0305a7b06d156f1094
Oscar Gichana <ossygicchy@gmail.com>
2021-04-13 00:36:12 +0300

    adding admin to database

---
+++ tracker/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Profile,Neighborhood,Posts,Business
+
+# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Neighborhood)
+admin.site.register(Posts)
+admin.site.register(Business)
+# admin.site.register(DisLike)
+# admin.site.register(MoringaMerch)
+# admin.site.register(AwardsProject)
