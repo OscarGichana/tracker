962b0acf7426d774b38079b4403f935e6e5334e1
Oscar Gichana <ossygicchy@gmail.com>
2021-04-13 00:38:48 +0300

    adding user update form

--- tracker/forms.py
+++ tracker/forms.py
@@ -23,3 +23,22 @@ class NewNeighborhoodForm(forms.ModelForm):
         exclude = ['editor', 'author_profile', 'user', 'likes', 'comment',]
 
 
+class UserEditView(generic.CreateView):
+    form_class = UserChangeForm
+    template_name = 'django_registration/edit_profile.html'
+    success_url = reverse_lazy('index')
+
+class UserUpdateForm(UserChangeForm):
+    password = None
+
+    class Meta:
+        model = Profile
+        fields = ['first_name', 'email', 'first_name', 'last_name', 'bio', 'phone_number',]
+        widgets = {
+            # 'user': models.OneToOneField(User, on_delete=models.CASCADE),
+            'bio': forms.Textarea(attrs={'rows': 3}),
+            'first_name': forms.TextInput(attrs={'rows': 1}),
+            'email': forms.TextInput(attrs={'rows': 1}),
+            'phone_number': forms.NumberInput(attrs={'rows': 1}),
+            'location': forms.TextInput(attrs={'rows': 1}),
+        }
