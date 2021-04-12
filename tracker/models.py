e7a4d084eb427d5ad7be22429efff6859d17c10c
Oscar Gichana <ossygicchy@gmail.com>
2021-04-13 00:34:19 +0300

    adding Neighborhood profile.

--- tracker/models.py
+++ tracker/models.py
@@ -42,3 +42,62 @@ def create_profile(sender, instance, created, **kwargs):
 
 post_save.connect(create_profile, sender = User)
 
+
+
+class Neighborhood(models.Model):
+    Admin = models.ForeignKey(User, on_delete=models.CASCADE, blank=True,null=True)
+    name = models.CharField(max_length = 60,null=True,blank=True)
+    location = models.CharField(max_length = 60,null=True,blank=True)
+    count = models.IntegerField(default=0)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    country = CountryField(blank_label='(select country)', default='KE')
+    health = models.CharField(max_length = 60,null=True,blank=True)
+    police = models.CharField(max_length = 60,null=True,blank=True)
+    fire = models.CharField(max_length = 60,null=True,blank=True)
+    pic = CloudinaryField('pic',null=True) 
+
+    def __str__(self):
+        return str(self.user.username)
+    class Meta:
+        ordering = ['name']
+    def create_neighborhood(self):
+        self.save()
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def get_neighborhoods(cls):
+        projects = cls.objects.all()
+        return projects
+    
+    @classmethod
+    def search_neighborhoods(cls, search_term):
+        projects = cls.objects.filter(name__icontains=search_term)
+        return projects
+    
+    
+    @classmethod
+    def get_by_admin(cls, Admin):
+        projects = cls.objects.filter(Admin=Admin)
+        return projects
+    
+    
+    @classmethod
+    def get_neighborhood(request, neighborhood):
+        try:
+            project = Neighborhood.objects.get(pk = id)
+            
+        except ObjectDoesNotExist:
+            raise Http404()
+        
+        return project
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'My Neighborhood'
+        verbose_name_plural = 'Neighborhoods'
+
+
